8a6a8ff9535285846e64a410503c07ecc0f6894e
Sebastián Cervera Maltos <117860901+sebascm0906@users.noreply.github.com>
2025-02-24 07:52:19 -0600

    cambios

---
+++ src/index.js
@@ -0,0 +1,15 @@
+function calculate(){
+    const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
+
+    let result = parseInt(num1) + parseInt(num2);
+
+    document.getElementById('result').innerHTML = 'Result: ' + result;
+}
+
+function resetFields(){
+    document.getElementById('num1').value = '';
+    document.getElementById('num2').value = '';
+    document.getElementById('result').innerHTML = 'Result: ';
+}   
+
