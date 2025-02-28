792acecdcdc826da4dca5e0f68f6b4b2d6522763
Sebastián Cervera Maltos <117860901+sebascm0906@users.noreply.github.com>
2025-02-27 23:19:48 -0600

    m

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ function calculatePlus(){
 
     document.getElementById('result').innerHTML = 'Result: ' + result;
 }
-function calculateMinus(){
-    const num1 = document.getElementById('num1').value;
-    const num2 = document.getElementById('num2').value;
+// function calculateMinus(){
+//     const num1 = document.getElementById('num1').value;
+//     const num2 = document.getElementById('num2').value;
 
-    let result = parseInt(num1) - parseInt(num2);
+//     let result = parseInt(num1) - parseInt(num2);
 
-    document.getElementById('result').innerHTML = 'Result: ' + result;
-}
+//     document.getElementById('result').innerHTML = 'Result: ' + result;
+// }
 
 function resetFields(){
     document.getElementById('num1').value = '';
